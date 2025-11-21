855970ed037e97ba5fbfd15682679d728e2c2a04
Marcosv21 <137106075+Marcosv21@users.noreply.github.com>
2025-11-21 14:32:43 -0300

    Update and rename FLASH.sh to flash.sh

---
+++ Etapa pré analise/flash.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Dependencies: FLASH
+# Install:
+#   conda install -c bioconda flash
+# Activate the Conda environment -- if need be
+eval "$(conda shell.bash hook)"
+conda activate flash
+
+FASTQ_PATH = "/media/marcos/TRABALHO/PRJEB59406/fastp_filtered" #Pathway of the archive filtered (fastp)
+OUTPUT_PATH_BASE = "/media/marcos/TRABALHO/PRJEB59406/flash_merged" 
+
+mkdir -p "$OUTPUT_PATH"
+
+# Loop through all *_R1.fastq files in the FASTQ_PATH
+for FILE1 in "$FASTQ_PATH"/*_R1.fastq; do
+  # Get the base name of the sample (remove the _R1.fastq suffix)
+  BASENAME=$(basename "$FILE1" _R1.fastq)
+
+  # Define the corresponding file for the _R2.fastq pair
+  FILE2="${FASTQ_PATH}/${BASENAME}_R2.fastq"
+
+  # Check if the _R2.fastq file exists
+  if [ -f "$FILE2" ]; then
+    # Merging with FLASH
+    flash -m 20 -M 150 --to-stdout "$FILE1" "$FILE2" > "${OUTPUT_PATH}/${BASENAME}.merged.fastq" 
+    # -m 20: based on the minimum merge of fastp, which is 30, and the default of FLASH, which is 10.
+    # -M 150: avoids the WARNING from FLASH saying that many sequences overlap more than the parameter, but it didn't change the merge percentage
+  else
+    echo "Pair for file $FILE1 not found: $FILE2" >&2
+  fi
+done
